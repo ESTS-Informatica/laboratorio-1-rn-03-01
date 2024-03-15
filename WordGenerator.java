97722e87d1fada5640d4a9dfc69fe6f901427766
JeanFerOliv <202300095@estudantes.ips.pt>
2024-03-15 10:31:21 +0000

    criação da classe Word generator

---
+++ WordGenerator.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+/**
+ * Write a description of class WordGenerator here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class WordGenerator
+{
+    // instance variables - replace the example below with your own
+    private ArrayList<String> words;
+    
+    private void fillArrayList()
+    {
+        
+    }
+}
